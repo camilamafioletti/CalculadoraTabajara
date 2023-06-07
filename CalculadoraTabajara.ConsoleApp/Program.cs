bab3451ebca318a2ba61c7a60b6323fd83908849
Camila <camilamafioletti01@gmail.com>
2023-06-07 15:10:47 -0300

    Implementado base da calculadora

--- CalculadoraTabajara.ConsoleApp/Program.cs
+++ CalculadoraTabajara.ConsoleApp/Program.cs
@@ -4,9 +4,119 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("OPA");
-            Console.WriteLine("Salve");
+            string opcao;
+
+            List<String> operacoes = new List<string>();
+
+            while (true) 
+            {
+                Console.WriteLine("Calculadora Tajabara 1.0\n");
+
+                Console.WriteLine("Tela Principal\n");
+
+                Console.WriteLine("Digite 1 para Adição\n");
+
+                Console.WriteLine("Digite 2 para Subtração\n");
+
+
+
+                Console.WriteLine("Digite 5 para Visualizar as operações\n");
+
+                Console.WriteLine("Digite S para sair\n");
+
+                Console.WriteLine("Opção: ");
+                opcao = Console.ReadLine();
+
+
+                #region calculo
+
+                Console.Clear();
+
+                Console.WriteLine("Calculadora Tabajara 1.0\n");
+
+                string operacao = "";
+
+                switch (opcao)
+                {
+                    case "1": operacao = "Adição"; break;
+
+                    case "2": operacao = "Subtração"; break;
+
+                    case "5":
+                        {
+                            Console.WriteLine("Visualizando operações realizadas: \n");
+
+                            foreach (string op in operacoes)
+                            {
+                                Console.WriteLine(op);
+                            }
+
+                            Console.ReadLine();
+                        };
+                        continue;
+
+                    default:
+                        break;
+                }
+
+                #endregion
+
+                #region resultado
+
+                Console.Write($"Digite o primeiro número: ");
+                double primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write($"Digite o segundo número: ");
+                double segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+                double resultado = 0;
+
+                switch (opcao)
+                {
+                    case "1": resultado = primeiroNumero + segundoNumero; break;
+
+                    case "2": resultado = primeiroNumero - segundoNumero; break;
+
+
+
+                    default:
+                        break;
+                }
+
+                Console.Clear();
+
+                Console.WriteLine("Tela de Resultados\n");
+
+                Console.Write($"O resultado da operação de {operacao} é: {resultado}");
+
+                string simbolo = "";
+                switch (opcao)
+                {
+                    case "1": simbolo = "+"; break;
+
+                    case "2": simbolo = "-"; break;
+
+
+
+
+                    default:
+                        break;
+                }
+
+                string descricaoOperacao = $"{primeiroNumero} {simbolo} {segundoNumero} = {resultado}";
+
+                operacoes.Add(descricaoOperacao);
+
+                Console.WriteLine();
+
+                Console.ReadLine();
+
+                Console.Clear();
+
+                #endregion
+            }
+
+
         }
     }
 }
